2b2de73f57bcb57d6e0c38c5dbbc0ba181dcf543
x-state-7 <chaudharyhemantkumar030@gmail.com>
2025-05-18 15:41:32 +0530

    created route for runAnalysis Controller

--- server/routes/FileUpload.js
+++ server/routes/FileUpload.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const { localFileUpload } = require("../controller/fileUpload")
-const { callName } = require("../controller/testScript")
+// const { callName } = require("../controller/testScript")
+const { runAnalysis } = require('../controller/runAnalysis')
+
 
 // creating API Route
 router.post("/analyze", localFileUpload)
-router.get("/name", callName)
+router.post("/result", runAnalysis)
+// router.get("/name", callName)
 
 module.exports = router
